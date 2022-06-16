a342cf1d41c0a42aad7386b25fe2cb28d3f43cc8
fritzsima <zh19940321@gmail.com>
2022-06-16 09:17:25 +0800

    feat: added apis for facebook

--- src/services/services/facebook.service.js
+++ src/services/services/facebook.service.js
@@ -9,6 +9,28 @@ async function sendMessage(data) {
     // Check data.contactList: data is req.body
     // return a array of true or false with contact list (sucessfully sent or not)
     let result=[];
+
+    const PAGE_ACCESS_TOKEN = 'page_access_token'
+
+    for (let chatId of data.contactList) {
+        let url = 'https://graph.facebook.com/v14.0/me/messages';
+        url += `?access_token=${PAGE_ACCESS_TOKEN}`;
+
+        const body = {
+            recipient: {
+                id: chatId
+            },
+            message: {
+                text: data.message
+            }
+        }
+
+        const response = await fetch(url, {method: 'post', body: JSON.stringify(body)});
+        const resultPerMsg = response.json();
+
+        result.push(resultPerMsg);
+    }
+
     return result;
 }
 
